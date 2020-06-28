48e94368edd83e0662894c1fae0e268ac36facfb
lamdam <you@example.com>
2020-06-28 20:12:42 +0200

    progressing in pipelien ...

--- main_script.py
+++ main_script.py
@@ -1,18 +1,31 @@
 import argparse
-# from p_acquisition import m_acquisition as m_ac
+from p_acquisition import m_acquisition as m_ac
 
+def greet():
+    ## Dejo esto porque me sirve para saber donde ando
+    print('Hello World!')
+
+
+########################################## pendiente utilizar argparse
 def argument_parser():
     parser = argparse.ArgumentParser(description = 'Set chart type')
+
+    # argumentos a pasar
     parser.add_argument('-n', '--nombre', type = str, help = 'specify a name...')
+    parser.add_argument('-c_n', '--country_name', type = str, help= 'Specify an country or [All] ...')
     args = parser.parse_args()
     return args
 
 def main(arguments):
     print(f' print arf_nombre: {arguments.nombre}')
     
+##########################################
 
 if __name__ == '__main__':
-    """year = int(input('Enter the year: '))
-    title = 'Top 10 Manufacturers by Fuel Efficiency ' + str(year) """
+    """
+    year = int(input('Enter the year: '))
+    title = 'Top 10 Manufacturers by Fuel Efficiency ' + str(year)
     arguments = argument_parser()
     main(arguments)
+    """
+    acquire_raw_data()
--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
 # acquisition functions
+## all in here
 
-def acquire():
-    data = pd.read_csv('./data/raw/vehicles.csv')
+def acquire_raw_data():
+    print(f' -> Getting data ...')
+    data = pd.read_csv('./data/raw/raw_data_project_m1.db')
+    print(f' -> Getting raw_data Done!')
     return data
+
+def saving_processed_data():
+    # print(f' -> Saving..')
+    # print(f' -> Getting raw_data Done!')
+    pass
+
+
+
+##########################
+
