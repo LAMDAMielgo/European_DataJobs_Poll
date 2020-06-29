edfcfaf67cefbf807f436319feaa78c02d60ad4e
lamdam <you@example.com>
2020-06-29 14:56:01 +0200

    Advancing with pipeline.. done until poll cleaning

--- main_script.py
+++ main_script.py
@@ -1,31 +1,238 @@
 import argparse
+import sys
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import re
+from datetime import datetime
+from sklearn.preprocessing import OneHotEncoder
+from bs4 import BeautifulSoup
+from functools import reduce
+
 from p_acquisition import m_acquisition as m_ac
-
-def greet():
-    ## Dejo esto porque me sirve para saber donde ando
-    print('Hello World!')
+from p_wrangling import m_wrangling as m_wr
 
 
-########################################## pendiente utilizar argparse
 def argument_parser():
-    parser = argparse.ArgumentParser(description = 'Set chart type')
+    """
+    Allows for inputs when running main_script.py in terminal
+    """
+    parser = argparse.ArgumentParser(description = 'Specify input DB file and API key...')
 
-    # argumentos a pasar
-    parser.add_argument('-n', '--nombre', type = str, help = 'specify a name...')
-    parser.add_argument('-c_n', '--country_name', type = str, help= 'Specify an country or [All] ...')
+    # Defined arguments
+    parser.add_argument('-p', '--path', type = str, help = 'specify the raw data path...')
+    parser.add_argument('-k', '--api_key', type = str, help= 'specify an Quandl API Key to identify user...')
+
+    # Arguments parser
     args = parser.parse_args()
     return args
 
-def main(arguments):
-    print(f' print arf_nombre: {arguments.nombre}')
-    
-##########################################
+#-------------------------------------------------------------------------------- acquiring processed data
+def acquire_career_info(df_career_info):
+    """Career info is list_of_dfs[0]"""
+    try:
+        print(f'\n\n· Cleaning df_career_info ....')
+        # Binarizing data
+        df_career_info['dem_full_time_job'] = m_ac.yes_no_to_bool(df_career_info['dem_full_time_job'])
+
+        # Changing nulls by unknown (qualitative params [low, medium, high, unknown])
+        df_career_info['dem_education_level'] = m_ac.null_to_unknown(df_career_info['dem_education_level'])
+
+        # Separate cols for boolean options
+        initial_cols = ['dem_education_level']
+        final_cols = ['High_Ed', 'Low_Ed', 'Medium_Ed', 'No_Ed', 'Unknown_Ed']
+
+        new_bool_df = m_ac.separate_df_to_bools(df_career_info, initial_cols, final_cols)
+        df_career_info = df_career_info.join(other=new_bool_df, on=None, how='left', sort=False)
+
+        # Dropping duplicate cols
+        cols_to_del = ['dem_education_level', 'normalized_job_code']
+        df_career_info.drop(columns=cols_to_del, inplace=True)
+
+        # Save table into local folder
+        m_ac.save_df_to_csv(df_career_info,
+                       path='data/processed',    # Function adds hierarchy of files
+                       name=f'career_info')      # Name of csv
+
+    except:
+        print('Something went wrong with [acquire_table_career_info]')
+
+    finally:
+        # Memory Usage and objects manually from Jupyter file
+        print('''\t\t\t  >> Done cleaning df_career_info!. 
+        >>> MEMORY USAGE \t FROM 301.7+ KB to 207.4+ KB
+        >>> DATA \t FROM objects(4) TO bool(6), object(2)
+                 \t\t\t  >> Chekout /data/processed/''')
+
+def acquire_country_info(df_country_info):
+    """Country info is list_of_dfs[1]"""
+    try:
+        print(f'\n\n· Cleaning df_country_info ....')
+        # String Operations multiple inputs to binomial cols -> only 2 values from 2 options
+        df_country_info['rural'] = m_ac.context_homogenization(df_country_info['rural'])
+
+        # Separate cols for boolean options
+        initial_cols = ['rural']
+        final_cols = ['rural_context', 'urban_context']
+
+        new_bool_df = m_ac.separate_df_to_bools(df_country_info, initial_cols, final_cols)
+        df_country_info = df_country_info.join(other=new_bool_df,
+                                               on=None,
+                                               how='left',
+                                               sort=False)
+
+        df_country_info.drop(columns='rural', inplace=True)
+
+        # Save table into local folder
+        m_ac.save_df_to_csv(df_country_info,
+                       path='data/processed',    # Function adds hierarchy of files
+                       name=f'country_info')     # Name of csv
+
+    except:
+        print('Something went wrong with [acquire_career_info]')
+
+    finally:
+        print('''\t\t\t  >> Done cleaning df_country_info!.
+        >>> MEMORY USAGE \t FROM 337.0+ KB to 226.3+ KB
+        >>> DATA \t FROM objects(5) TO object(3)
+                 \t\t\t  >> Chekout /data/processed/''')
+
+def acquire_personal_info(df_personal_info):
+    """Personal info is list_of-dfs[2]"""
+
+    try:
+        print(f'\n\n· Cleaning df_personal_info ....')
+        # Number normalization
+        df_personal_info['age'] = m_ac.ageStr_to_ageNum(serie= df_personal_info['age'])
+        df_personal_info['age'] = m_ac.year_to_age(df_personal_info['age'])
+        # df_personal_info['age'] = m_ac.year_update(df_personal_info['age']) NOT DONE
+
+        # String Operations: multiple inputs in binomial cols -> only 2 values for 2 options
+        df_personal_info['gender'] = m_ac.gender_homogenization(df_personal_info['gender'])
+        df_personal_info['dem_has_children'] = m_ac.yes_no_to_bool(df_personal_info['dem_has_children'])
+
+        # Separate cols for boolean options
+        initial_cols = ['gender', 'age_group']
+        final_cols = ['gender_Female', 'gender_Male', 'ageGroup_14_25', 'ageGroup_26_39', 'ageGroup_40_65',
+                      'ageGroup_juvenile']
+
+        new_bool_df = m_ac.separate_df_to_bools(df_personal_info, initial_cols, final_cols)
+        df_personal_info = df_personal_info.join(other=new_bool_df, on=None, how='left', sort=False)
+
+        # Save table into local folder
+        m_ac.save_df_to_csv(df_personal_info,
+                       path='data/processed',    # Function adds hierarchy of files
+                       name=f'personal_info')    # Name of csv
+
+    except:
+        print('Something went wrong with [acquire_table_personal_info]') # Make a log file
+
+    finally:
+        print('''\t\t\t  >> Done cleaning df_personal_info!. 
+        >>> MEMORY USAGE \t FROM 337.0+ KB to 367.6+ KB
+        >>> DATA \t FROM objects(5) TO bool(7), object(4)
+                 \t\t\t  >> Chekout /data/processed/''')
+
+def acquire_poll_info(df_poll_info):
+    """Poll info is list_of-dfs[3]"""
+
+    sep = ' | '  # this could change
+    series = [
+        df_poll_info['question_bbi_2016wave4_basicincome_awareness'],
+        df_poll_info['question_bbi_2016wave4_basicincome_effect'],
+        df_poll_info['question_bbi_2016wave4_basicincome_vote'],
+        df_poll_info['question_bbi_2016wave4_basicincome_argumentsagainst'],
+        df_poll_info['question_bbi_2016wave4_basicincome_argumentsfor']
+        ]
+
+    try:
+        print(f'\n\n· Cleaning df_poll_info ....')
+        # Deleting strange characters in column ------> this should be as a def in m_ac
+        df_poll_info['question_bbi_2016wave4_basicincome_effect'] = [m_ac.split_str_at_char(response, 'Û_ ')
+                                                                     if re.search('Û_ ', response)
+                                                                     else response
+                                                                     for response in df_poll_info[
+                                                                         'question_bbi_2016wave4_basicincome_effect']
+                                                                     ]
+
+        # Separating AWARENESS POLL -----------> me he quedado aquí
+        print(f'..Separating polls')
+        df_poll_basicincome_awareness = m_ac.multiple_choice_col_to_df(serie= series[0], separator= sep)
+        m_ac.save_df_to_csv(df_poll_basicincome_awareness,
+                       path='data/processed',                   # Function adds hierarchy of files
+                       name=f'poll_basicincome_awareness')      # Name of csv
+
+        df_poll_basicincome_effect = m_ac.multiple_choice_col_to_df(serie= series[1], separator= sep)
+        df_poll_basicincome_vote = m_ac.multiple_choice_col_to_df(serie= series[2], separator= sep)
+        df_poll_basicincome_argumentsagainst = m_ac.multiple_choice_col_to_df(serie= series[3], separator= sep)
+        df_poll_basicincome_argumentsfor = m_ac.multiple_choice_col_to_df(serie= series[5], separator= sep)
+
+        print(f'..Beginning saving separated polls')
+        # Save tables into local folder
+
+
+        m_ac.save_df_to_csv(df_poll_basicincome_effect,
+                       path='data/processed',                   # Function adds hierarchy of files
+                       name=f'poll_basicincome_effect')         # Name of csv
+
+        m_ac.save_df_to_csv(df_poll_basicincome_vote,
+                       path='data/processed',                   # Function adds hierarchy of files
+                       name=f'poll_basicincome_vote')           # Name of csv
+
+        m_ac.save_df_to_csv(df_poll_basicincome_argumentsagainst,
+                       path='data/processed',                     # Function adds hierarchy of files
+                       name=f'poll_basicincome_argumentsagainst') # Name of csv
+
+        m_ac.save_df_to_csv(df_poll_basicincome_argumentsfor,
+                       path='data/processed',                   # Function adds hierarchy of files
+                       name=f'poll_basicincome_argumentsfor')   # Name of csv
+
+    except:
+        print('Something went wrong with [acquire_table_personal_info]') # Make a log file
+
+    finally:
+        print('''\t\t\t  >> Done cleaning df_poll_info!. 
+                 \t\t\t  >> Chekout /data/processed/''')
+
+#-------------------------------------------------------------------------------- main
+
+def main(some_args):
+    print('Beginning pipeline...')
+    print(f'\t\t -> arg_path parsed: {arguments.path}')
+    print(f'\t\t -> arg_key parsed : {arguments.api_key}')
+    print('done parsing arguments!')
+
+    # Now i should iterate through tables, clean them aaand save
+    table_names = m_ac.ddbb_tables(m_ac.connect_to_table(arguments.path))
+    list_of_dfs = m_ac.fetch_all_from_tables(engine= m_ac.connect_to_table(arguments.path),
+                                     table_names_from_ddbb= table_names)  #['career_info', 'country_info', 'personal_info', 'poll_info'] as dfs
+    return list_of_dfs
+
 
 if __name__ == '__main__':
+
     """
-    year = int(input('Enter the year: '))
-    title = 'Top 10 Manufacturers by Fuel Efficiency ' + str(year)
+    # Information ddbb
+    year_ddbb = '2016'
+    year_change = int(input('Enter the year: '))
+    title = 'DATA JOBS BY GENDER ' + year_ddbb
+    print(main(arguments)[0])  --> esto devuelve career_info, nos entendemos??
+    """
+
+    # Using arguments
     arguments = argument_parser()
     main(arguments)
-    """
-    acquire_raw_data()
+
+    # Getting DataFrames from connection
+    list_of_dfs_from_ddbb = main(arguments)
+
+    # Processing raw_data to analize and saving in local /data/processed
+    # ['career_info', 'country_info', 'personal_info', 'poll_info']
+    df_career_info = acquire_career_info(list_of_dfs_from_ddbb[0])
+    df_country_info = acquire_country_info(list_of_dfs_from_ddbb[1])
+    df_personal_info = acquire_personal_info(list_of_dfs_from_ddbb[2])
+    df_poll_info =acquire_poll_info(list_of_dfs_from_ddbb[3])
+
+    # By here DFs have to be saved in folder /data/processed
--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,20 +1,239 @@
+from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+import re
+from datetime import datetime
+from sklearn.preprocessing import OneHotEncoder
+from functools import reduce
 
-# acquisition functions
-## all in here
+#-------------------------------------------------------------------------------- functions for stablishing connection t ddbb
 
-def acquire_raw_data():
-    print(f' -> Getting data ...')
-    data = pd.read_csv('./data/raw/raw_data_project_m1.db')
-    print(f' -> Getting raw_data Done!')
-    return data
+def connect_to_table(ddbb):
+    print(f'Creating DDBB connection..')
+    """In case it doesn't work : 'sqlite:////home/lucia/Descargas/raw_data_project_m1.db'"""
+    engine = create_engine(f'sqlite:///{ddbb}')
+    connection = engine.connect()
+    print(f'''Created connection with DDBB
+              Table names -> \t{engine.table_names()}''')
+    return engine
 
-def saving_processed_data():
-    # print(f' -> Saving..')
-    # print(f' -> Getting raw_data Done!')
+def ddbb_tables(engine):
+    """
+    Given a connection to a ddbb return tables as list
+    """
+    print(f'Getting table names...')
+
+    connection = engine.connect()
+    ddbb_table_names = engine.table_names()
+
+    print(f'Done. Table names -> {ddbb_table_names}')
+    return ddbb_table_names
+
+def fetch_all_from_tables(engine, table_names_from_ddbb):
+    """
+    Saves each table from DDBB to a DF
+    """
+    list_of_df =  []
+    for table in table_names_from_ddbb:
+        table_df = pd.read_sql_table(table_name=table,
+                                     con= engine)
+        list_of_df.append(table_df)
+
+    return list_of_df
+
+#-------------------------------------------------------------------------------- functions for number standarization
+
+def ageStr_to_ageNum(serie):
+    """
+    INPUT -> serie df[] = ['61 years old', '57 years old', '32 years old'] -> full strings
+    OUPUT -> serie df[] = [ 61 57 32 45 41 1990 2000]                      -> only integers
+    """
+    print(f' ·· Transform strings into integers')
+    serie = serie.apply(lambda x: re.sub('\D', '', x)).astype(int)
+    return serie
+
+def year_to_age(serie):
+    """
+    INPUT -> serie:  df[] = [ 61 57 32 45 41 1990 2000]   -> ages + years (all ints)
+    OUTPUT -> serie: df[] = [ 61 57 32 45 41   30   20]   -> only ages    (all ints)
+    """
+    print(f' ·· Evaluating integers as [age]')
+    year_db = 2016  # DB is from this year!
+    serie = serie.apply(lambda x: year_db - x if int(x) > 200 else x)
+    return serie
+
+def year_update(serie):
+    """
+    La tabla está en 2016, hay que actualizar datos para uqe no haya incongruencias entre Age y Age_Group
+    """
+    print(f' ·· [year_update] doing its magic')
+    year_now = datetime.today().year
+    year_db = 2016
+    serie = serie.apply(lambda x: (year_now - year_db) + x)
+    return serie
+
+#-------------------------------------------------------------------------------- functions for string standarization
+
+def null_to_unknown(serie):
+    """
+    INPUT  -> no  high     None  medium     None  low  no
+    OUTPUT -> no  high  unknown  medium  unknown  low  no
+    """
+    print(f' ·· Transform [None] to [Unknown]') # Data interpretation here
+    serie = serie.apply(lambda x: 'unknown' if x == None else x)
+    return serie
+
+def gender_homogenization(serie):
+    """
+    INPUT  -> female, FeMale, Fem, male, Male
+    OUTPUT ->      F,      F,   F,    M,    M
+    """
+    print(f' ·· Binarazing [gender]')
+    serie = serie.apply(lambda x: re.sub('^f\w+|^F\w+', 'F', x))
+    serie = serie.apply(lambda x: re.sub('^m\w+|^M\w+', 'M', x))
+    return serie
+
+def context_homogenization(serie):
+    """
+    INPUT  ->   urban  city  rural  Non-Rural Countryside  -> various types of response
+    OUTPUT ->   urban  urban rural      urban       rural  -> two types of response
+    ------------------------------------------------------------------------------------
+    LIST OF POSSIBLE ANSWERS TAKING INTO ACCOUNT serie count values
+    """
+    print(' ·· Binarizing [context]')
+    urban_context = ['urban', 'city', 'non-rural', 'Non-Rural']
+    rural_context = ['rural', 'country', 'countryside', 'Country']
+
+    return ['urban_context' if response in urban_context
+            else 'rural_context' if response in rural_context
+            else None
+            for response in serie]
+
+def split_str_at_char(str_to_split, cutter):
+    """
+    Separates a string into a list of strings by string (=cutter)
+    --------------------------------------------------------------------------------------
+    INPUT:    ['Hola que tal | soy Pepito | Hace un día estupendo']    --> list w one string
+    OUTPUT:   [ 'Hola que tal', 'soy Pepito', 'Hace un día estupendo'] --> list w n string
+    """
+    if isinstance(str_to_split, str) and isinstance(cutter, str):
+        return re.split(cutter, str_to_split)[1].capitalize()
+    else:
+        print('EntryError [at split_str_at_char]: wrong type of inputs')
+
+def yes_no_to_bool(serie):
+    """
+    Appliable to yes/no questions with multiple formats, to transform into boolean info
+    INPUT  -> YES yes Yes yES No NO nO no  -> type str
+    OUTPUT ->   1   1   1   1  0  0  0  0  -> type bool
+    """
+    print(f' ·· Binarizing [yes/no answers]')
+    serie = serie.apply(lambda x: re.sub('^y\w+|^Y\w+', '1', x))
+    serie = serie.apply(lambda x: re.sub('^n\w+|^N\w+', '0', x)).astype(int)
+    return serie.astype(bool)
+
+#-------------------------------------------------------------------------------- functions for number standarization
+
+def separate_df_to_bools(df, cols_to_separate, cols_separated):
+    """
+    INPUT  -> df[col].unique() = [range_1, range_2, range_3]
+    OUTPUT -> df[[range_1, range_2, range_3]] with boolean responses
+    """
+    print(f' ·· Separating binary columns')
+    df_encoder = OneHotEncoder(dtype=bool, sparse=True)
+    df = pd.DataFrame(df_encoder.fit_transform(df[cols_to_separate]).toarray(), columns=cols_separated)
+    return df
+
+#---------------------------------- all of this is only for polls_info!
+
+def get_uniqueResponses(serie, separator):
+    """
+    This function searches for the uniques responses in a multiple choice response
+    presented as a concatenated string in one column.
+    ----------------------------------------------------------------------------------
+    INPUT  ->  'E | F | C | D'   'D | A'    'C'   'E | B'  --> Unsorted concat strings
+    OUTPUT ->  ['E', 'F', 'C', 'D', 'A', 'B']              --> Unsorted unique strings
+
+    """
+    try:
+        if isinstance(separator, str):
+            list_of_all_responses = set()
+
+            print(f' ··· Getting Unique values in [poll_column]')
+            flattened_list_of_responses = reduce(lambda x, y: x + y,
+                                                 [item.split(separator) for item in serie.unique()])
+
+            for response in flattened_list_of_responses:
+                if response not in list_of_all_responses:
+                    list_of_all_responses.add(response)
+
+            return list(list_of_all_responses)
+        else:
+            print('EntryError [at get_uniqueResponses]: wrong type of inputs')
+
+    except:
+        print('Exception at [get_uniqueResponses]')
+
+def to_binary_matrix_of_equals(list_uniques, list_to_eval):
+    """
+    This returns multiple strings in an boolean form, evaluated against all options
+    Making very easily to transform each list_to_eval into a DF of its own
+    -------------------------------------------------------------------------------
+    INPUT:   uniques [opcion 1, opcion 2, opcion 3]
+           list eval [[option 1], [opcion 1, opcion 3], [opcion1, opcion 3, opcion 2]]
+
+    OUTPUT:   matrix [[1,0,0], [1,0,1], [1,1,1]]
+    """
+    print(f' ··· Initiating binary matrix of  [poll_column]')
+
+    list_uniques_lenghts = [len(i) for i in list_uniques]
+
+    list_to_eval_iterabl = iter(list_to_eval)
+    list_to_eval_lenghts = list(reduce(lambda x, y: x + y, list_to_eval))
+
+    list_of_lists = []
+
+    try:
+        print(f'\t ···· Iterating through lists')
+        for v in list_to_eval_lenghts:
+            arr = [len(i) for i in next(list_to_eval_iterabl)]
+            list_of_arrays = []
+
+            for len_num in arr:
+                list_of_arrays.append(np.where(np.array(list_uniques_lenghts) == len_num, 1, 0))
+
+            list_of_lists.append(list_of_arrays)
+
+    except StopIteration:
+        pass
+
+    print(f'\t  ··· Iteration ended. Exited loop')
+    flat_arrays = [np.sum(i, axis=0) for i in list_of_lists]
+    binary_matrix = [i.tolist() for i in flat_arrays]
+
+    return binary_matrix
+
+def multiple_choice_col_to_df(serie, separator):
+    """
+    Makes all the operations to return a boolean df with all the possible responses from each poll
+    Nested functions : get_uniqueResponses()   to_binany_matrix_of_equals()
+    """
+    print(f'\n ·· Creating DFs from [poll_column]')
+    poll_info_allResponses = get_uniqueResponses(serie, separator)
+    graph_list_of_responses = serie.apply(lambda x: x.split(separator))
+    bin_matrix = to_binary_matrix_of_equals(poll_info_allResponses, graph_list_of_responses)
+    df = pd.DataFrame(bin_matrix, columns=poll_info_allResponses, dtype='bool')
+    print(f' ·· DF for answers created')
+
+    return df
+
+#-------------------------------------------------------------------------------- functions to save outputs
+
+def save_df_to_csv(df, path, name):
+    print(f' ·· Saving df in {path} as {name}')
+    path = './' + f'{path}'
+    return df.to_csv(f'{path}/{name}.csv')
+
+def save_img_to_folder():
     pass
-
-
-
-##########################
 
